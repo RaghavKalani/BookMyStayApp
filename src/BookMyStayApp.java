8d9207a97fcbd2970bb07a1084117fafcab2ce96
RaghavKalani <raghavkalani2225@gmai.com>
2026-03-10 09:42:55 +0530

    UC2: Basic Room Types & Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,6 +1,63 @@
-public class BookMyStayApp {
+abstract class Room {
+    protected String type;
+    protected int beds;
+    protected int size;
+    protected double price;
+
+    public Room(String type, int beds, int size, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println(type + ":");
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sqft");
+        System.out.println("Price per night: " + price);
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 250, 1500);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 400, 2500);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 750, 5000);
+    }
+}
+
+public class BookMyStayApp{
     public static void main(String[] args) {
-        System.out.println("Welcome to Hotel Booking Management System");
-        System.out.println("System initialized successfully.");
+        int singleRoomAvailable = 5;
+        int doubleRoomAvailable = 3;
+        int suiteRoomAvailable = 2;
+
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        System.out.println("Hotel Room Initialization\n");
+
+        single.displayDetails();
+        System.out.println("Available: " + singleRoomAvailable);
+        System.out.println();
+
+        doubleRoom.displayDetails();
+        System.out.println("Available: " + doubleRoomAvailable);
+        System.out.println();
+
+        suite.displayDetails();
+        System.out.println("Available: " + suiteRoomAvailable);
     }
 }
